67d00f82b62bfde25ab87173bba554ce1ee177af
WabukoWabuko <basilwabbs@gmail.com>
2026-08-07 22:17:24 +0300

    refactor: Clean up using directives in AuditService and update placeholder text in MainWindow

--- TextileWarehouseERP/Services/AuditService.cs
+++ TextileWarehouseERP/Services/AuditService.cs
@@ -1,9 +1,5 @@
 using System;
 using System.Threading.Tasks;
-using System;
-using System.Threading.Tasks;
-using System;
-using System.Threading.Tasks;
 using TextileWarehouseERP.Data;
 using TextileWarehouseERP.Models;
 
